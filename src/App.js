1d85a19790738eb6395b0787a6c35e85e6d94d52
Architv27 <archit.sfu@gmail.com>
2023-12-30 16:04:51 -0800

    homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import AboutMe from './AboutMe';
+
+const Projects = () => <div>Projects Content</div>;
+const Contact = () => <div>Contact Content</div>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="heroBackground">
+        <div className="titleContainer">
+            <div className="line"></div>
+              <h1>Archit Verma</h1>
+            <div className="line"></div>
+          </div>
+          <nav className="navbar">
+            <Link to="/about-me" className="navItem">About Me</Link>
+            <Link to="/projects" className="navItem">Projects</Link>
+            <Link to="/contact" className="navItem">Contact</Link>
+          </nav>
+          <div className="circle" />
+          <div className="circle small" />
+          <div className="circle medium" />
+          <div className="circle large" />
+          {/* Additional content here */}
+        </div>
+
+        <Routes>
+          <Route path="/about-me" element={<AboutMe />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
